8d1c7e3fe3523d68b91262c7209ad3c1dcf37c64
Johnny Dong <johnnydong@dhcp-128-189-68-78.ubcsecure.wireless.ubc.ca>
2025-12-06 11:24:33 -0800

    similar

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,6 +1,8 @@
+// @ts-ignore - explicit extension needed for CI/CD build resolution
+import { auth } from "./auth/resource.ts";
+// @ts-ignore
+import { data } from "./data/resource.ts";
 import { defineBackend } from "@aws-amplify/backend";
-import { auth } from "./auth/resource";
-import { data } from "./data/resource";
 
 defineBackend({
   auth,
